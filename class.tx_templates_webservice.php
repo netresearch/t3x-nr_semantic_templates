d93837e82e565b150230bdac6765a1428a52229a
Raphael Döhring <raphael.doehring@netresearch.de>
2009-10-16 12:31:44 +0000

    finished user parameters edit field. eval of field not finishd, coding style corrections
    git-svn-id: svn://svn.nr/typo3-extensions/nr_semantic_templates/trunk@46 d9bb3c8b-0657-4550-8ffb-3bcc9f996d49

--- class.tx_templates_webservice.php
+++ class.tx_templates_webservice.php
@@ -1,12 +1,28 @@
 <?php
+/**
+ * This file contains the semantic templates webservice class.
+ *
+ * PHP version 5
+ *
+ * @category   Netresearch
+ * @package    TYPO3
+ * @subpackage Tx_Nr_Semantictemplates
+ * @author     Raphael Doehring <raphael.doehring@googlemail.com>
+ * @license    No license http://www.netresearch.de
+ * @link       http://www.netresearch.de
+ */
 
 /**
- * Accommodates dynamic functions for the backend part of the Semantic Templates
- * extension. 
+ * Accommodates function to access the Semantic Templates webservice.
  *
- * @author Raphael Doehring (raphael dot doehring at googlemail dot com)
+ * @category   Netresearch
+ * @package    TYPO3
+ * @subpackage Tx_Nr_Semantictemplates
+ * @author     Raphael Doehring <raphael.doehring@googlemail.com>
+ * @license    No license http://www.netresearch.de
+ * @link       http://www.netresearch.de
  */
-class tx_templates_webservice
+class Tx_Templates_Webservice
 {
 
     /**
@@ -14,12 +30,12 @@ class tx_templates_webservice
      * web service. If an error occured it will be returned as label of the only
      * select box option.
      * 
-     * @param $config the flexform data
+     * @param mixed $config the flexform data
+     * 
      * @return array the select box options
      */
     function getTemplateNames($config)
-    {
-        $flexFormDataArray = t3lib_div::xml2array($config['row']['pi_flexform']);
+    {        $flexFormDataArray = t3lib_div::xml2array($config['row']['pi_flexform']);
         $lessUrl = '';
         if (is_array($flexFormDataArray) && ! empty($flexFormDataArray['data']['sDEF']['lDEF']['less_url']['vDEF'])) {
             $lessUrl = $flexFormDataArray['data']['sDEF']['lDEF']['less_url']['vDEF'];
@@ -33,41 +49,38 @@ class tx_templates_webservice
         }
 
         $optionList = array();
-        if(filter_var($lessUrl, FILTER_VALIDATE_URL)) {
+        if (filter_var($lessUrl, FILTER_VALIDATE_URL)) {
             $urlLength = strlen($lessUrl);
-            if('/' !== substr($lessUrl, $urlLength-1)) {
+            if ('/' !== substr($lessUrl, $urlLength-1)) {
                 $lessUrl .= '/';
             }
 
             // test web service response/reachability
             $testContent = file_get_contents($lessUrl . 'service/ping');
-            if('pong' === $testContent) {
+            if ('pong' === $testContent) {
                 $jsonContent = file_get_contents($lessUrl . 'service/list');
                 $templatesArray = json_decode($jsonContent);
 
                 // build select box options array
-                if(is_array($templatesArray)) {
-                    foreach($templatesArray as $currentTemplate) {
+                if (is_array($templatesArray)) {
+                    foreach ($templatesArray as $currentTemplate) {
                         
                         // @-thing is a hack to get the request type an the id
                         // in one select box
                         $value = $currentTemplate->requestType . '@' . $currentTemplate->id;
                         $optionList[] = array(0 => $currentTemplate->name,  1 => $value);
                     } // -- foreach template in array
-               } // -- if decoded result is an array
-               else {
-                   $optionList[] = array(0 => 'Got no data from web serivce.',  1 => $oldTemplateValue);
-               } // -- response array is empty
-            } // -- if challenge response is correct
-            else {
-                $optionList[] = array(0 => 'Server not reachable or not a LESS instance.',  1 => $oldTemplateValue);
+                } else {
+                    $optionList[] = array(0 => 'ERROR: Got no data from web serivce.',  1 => $oldTemplateValue);
+                } // -- response array is empty
+            } else {
+                $optionList[] = array(0 => 'ERROR: Server not reachable or not a LESS instance.',  1 => $oldTemplateValue);
             } // -- else, web service response is not as expected
-        } // -- if url is valid
-        else {
-            $optionList[] = array(0 => 'URL is not valid.',  1 => $oldTemplateValue);
+        } else {
+            $optionList[] = array(0 => 'ERROR: URL is not valid.',  1 => $oldTemplateValue);
         } // -- else, URL validation failed
 
-        $config['items'] = array_merge($config['items'],$optionList);
+        $config['items'] = array_merge($config['items'], $optionList);
         return $config;
 
     } // -- function getTemplateNames
@@ -75,7 +88,7 @@ class tx_templates_webservice
 } // -- class tx_templates_webservice
 
 if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/nr_sw_semantic_templates/class.tx_templates_webservice.php']) {
-    include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/nr_sw_semantic_templates/class.tx_templates_webservice.php']);
+    include_once $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/nr_sw_semantic_templates/class.tx_templates_webservice.php'];
 }
 
 ?>
--- pi1/class.tx_nrswsemantictemplates_pi1.php
+++ pi1/class.tx_nrswsemantictemplates_pi1.php
@@ -1,33 +1,18 @@
 <?php
-/***************************************************************
-*  Copyright notice
-*
-*  (c) 2009 Netresearch <info@netresearch.de>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
 /**
- * [CLASS/FUNCTION INDEX of SCRIPT]
+ * This file contains fe output for the semantic templates.
  *
- * Hint: use extdeveval to insert/update function index above.
+ * PHP version 5
+ *
+ * @category   Netresearch
+ * @package    TYPO3
+ * @subpackage Tx_Nr_Semantictemplates
+ * @author     Raphael Doehring <raphael.doehring@googlemail.com>
+ * @license    No license http://www.netresearch.de
+ * @link       http://www.netresearch.de
  */
 
-require_once(PATH_tslib.'class.tslib_pibase.php');
+require_once PATH_TSLIB.'class.tslib_pibase.php';
 
 
 /**
@@ -36,114 +21,138 @@ require_once(PATH_tslib.'class.tslib_pibase.php');
  * This plugin get the content of the semantic template from the web service
  * based on the data saved in the flexform and simply ouputs the returned string.
  *
- * @author	Netresearch <info@netresearch.de>
- * @package	TYPO3
- * @subpackage	tx_nrswsemantictemplates
+ * @category   Netresearch
+ * @package    TYPO3
+ * @subpackage Tx_Nr_Semantictemplates
+ * @author     Raphael Doehring <raphael.doehring@googlemail.com>
+ * @license    No license http://www.netresearch.de
+ * @link       http://www.netresearch.de
  */
-class tx_nrswsemantictemplates_pi1 extends tslib_pibase
+class Tx_Nrswsemantictemplates_Pi1 extends tslib_pibase
 {
-	var $prefixId      = 'tx_nrswsemantictemplates_pi1';		// Same as class name
-	var $scriptRelPath = 'pi1/class.tx_nrswsemantictemplates_pi1.php';	// Path to this script relative to the extension dir.
-	var $extKey        = 'nr_sw_semantic_templates';	// The extension key.
-	var $pi_checkCHash = true;
+    var $prefixId      = 'tx_nrswsemantictemplates_pi1';		// Same as class name
+    var $scriptRelPath = 'pi1/class.tx_nrswsemantictemplates_pi1.php';	// Path to this script relative to the extension dir.
+    var $extKey        = 'nr_sw_semantic_templates';	// The extension key.
+    var $pi_checkCHash = true;
 
 
-	/**
-	 * Reads the necessary data from the flexform, builds the web service
-         * request url and gets the data from the web serivce.
-	 *
-	 * @param	string		$content: The PlugIn content
-	 * @param	array		$conf: The PlugIn configuration
-	 * @return	The string containing the template output or an error message
-         *              if debug is enabled.
-	 */
-	function main($content, $conf)
-        {
-		$this->conf = $conf;
-		$this->pi_setPiVarDefaults();
-		$this->pi_loadLL();
-                $this->pi_initPIflexForm(); 
+    /**
+     * Reads the necessary data from the flexform, builds the web service
+     * request url and gets the data from the web serivce.
+     *
+     * @param string $content the PlugIn content
+     * @param array  $conf    the PlugIn configuration
+     *
+     * @return string The string containing the template output or an error message
+     *              if debug is enabled.
+     */
+    function main($content, $conf)
+    {
+        $this->conf = $conf;
+        $this->pi_setPiVarDefaults();
+        $this->pi_loadLL();
+        $this->pi_initPIflexForm();
 
-                // get and check parameters
-                $debugEnabledString = $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'debugEnabled');
-                $debugEnabled = false;
-                if(1 === $debugEnabledString) {
-                    $debugEnabled = true;
-                }
+        // ---------------  get and check parameters  ---------------
+        $debugEnabledString = $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'debugEnabled');
+        $debugEnabled = false;
+        if (1 === $debugEnabledString) {
+            $debugEnabled = true;
+        }
 
-                $templateIdString = $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'templateId');
-                $templateIdParts = split('@', $templateIdString);
-                if(count($templateIdParts) != 2) {
-                    if($debugEnabled) {
-                        return 'TemplateIdString has unexpected foramt: ' . $templateIdString;
-                    }
-                    return '';
-                }
-                $templateId = $templateIdParts[1];
+        $templateIdString = $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'templateId');
+        $templateIdParts = split('@', $templateIdString);
+        if (count($templateIdParts) != 2) {
+            if ($debugEnabled) {
+                return 'TemplateIdString has unexpected foramt: ' . $templateIdString;
+            }
+            return '';
+        }
+        $templateId = $templateIdParts[1];
 
-                $requestSpecificPart = '';
+        $requestSpecificPart = '';
 
-                if('uri' === $templateIdParts[0]) {
-                    $uri = $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'uri');
-                    $requestSpecificPart = 'uri=' . urlencode($uri);
-                }
-                else if('sparql' === $templateIdParts[0]) {
-                    $sparqlEndpoint = 
-                        urlencode($this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'sparqlEndpoint'));
-                    $sparqlQuery = 
-                        urlencode($this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'sparqlQuery'));
-                    $requestSpecificPart = 'sparqlEndpoint=' . $sparqlEndpoint
-                                    . '&sparqlQuery=' . $sparqlQuery;
-                }
-                else {
-                    if($debugEnabled) {
-                        return 'Neither uri nor sparql type found in templateIdString: ' . $templateIdString;
-                    }
-                    return '';
-                }
+        if ('uri' === $templateIdParts[0]) {
+            $uri = $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'uri');
+            $requestSpecificPart = 'uri=' . urlencode($uri);
+        } else if ('sparql' === $templateIdParts[0]) {
+            $sparqlEndpoint 
+                = urlencode($this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'sparqlEndpoint'));
+            $sparqlQuery 
+                = urlencode($this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'sparqlQuery'));
+            $requestSpecificPart = 'sparqlEndpoint=' . $sparqlEndpoint
+                            . '&sparqlQuery=' . $sparqlQuery;
+        } else {
+            if ($debugEnabled) {
+                return 'Neither uri nor sparql type found in templateIdString: ' . $templateIdString;
+            }
+            return '';
+        }
 
-                $lessUrl = $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'less_url');
-                if(!filter_var($lessUrl, FILTER_VALIDATE_URL)) {
-                    if($debugEnabled) {
-                        return 'URL to LESS instance is no valid URL: ' . $lessUrl;
-                    }
-                    return '';
-                }
+        $usersParametersString = $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'usersParameters');
+        // array holding the parsed parameters in the end
+        $usersParameters = array();
 
-                // if url doesn't end with / add it
-                $urlLength = strlen($lessUrl);
-                if('/' !== substr($lessUrl, $urlLength-1)) {
-                    $lessUrl .= '/';
-                }
+        // users parameters field is not empty
+        if ('' !== trim($usersParametersString)) {
+            $usersParametersLines = split(';', $usersParametersString);
+            if (count($usersParametersLines) > 0) {
+                // foreach key value pair
+                foreach ($usersParametersLines as $line) {
+                    if (preg_match('/(.*[^\\\]):(.*)/', $line, $matches)) {
+                        $usersParameters[trim($matches[1])] = trim(stripslashes($matches[2]));
+                    } // -- if its a match
+                } // -- foreach userParameters line
+            } // -- if there are more than 0 lines
+        } // -- if usersParameters box is not empty
 
-                // assemble url
-                $requestUrl = $lessUrl .
-                        'build?requestType=uri&templateId='
-                        . $templateId
-                        . '&' . $requestSpecificPart;
+        $lessUrl = $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'less_url');
+        if (!filter_var($lessUrl, FILTER_VALIDATE_URL)) {
+            if ($debugEnabled) {
+                return 'URL to LESS instance is no valid URL: ' . $lessUrl;
+            }
+            return '';
+        }
 
-                $debugEnabled = $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'debugEnabled');
-                if($debugEnabled) {
-                    $requestUrl .= '&debug=true';
-                }
+        // if url doesn't end with / add it
+        $urlLength = strlen($lessUrl);
+        if ('/' !== substr($lessUrl, $urlLength-1)) {
+            $lessUrl .= '/';
+        }
 
-                $returnValue = '';
-                if($debugEnabled) {
-                    $returnValue = 'url: ' . $requestUrl . '<br />';
-                }
+        // ---------------  assemble url  ---------------
+        $requestUrl = $lessUrl .
+                'build?requestType=uri&templateId='
+                . $templateId
+                . '&' . $requestSpecificPart;
 
-                // get content
-                $returnValue .= file_get_contents($requestUrl);
-		return $this->pi_wrapInBaseClass($returnValue);
-                
-	} // -- function main
+        $debugEnabled = $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'debugEnabled');
+        if ($debugEnabled) {
+            $requestUrl .= '&debug=true';
+        }
+
+        if (count($usersParameters) > 0) {
+            foreach ($usersParameters as $key => $value) {
+                $requestUrl .= '&parameter_' . $key . '=' . urlencode($value);
+            } // -- foreach parameter
+        } // -- if there are users parameters, append them to url
+
+        $returnValue = '';
+        if ($debugEnabled) {
+            $returnValue = 'url: ' . $requestUrl . '<br />';
+        }
+
+        // get content
+        $returnValue .= file_get_contents($requestUrl);
+        return $this->pi_wrapInBaseClass($returnValue);
+    } // -- function main
         
 } // -- class tx_nrswsemantictemplates_pi1
 
 
 // make sure class in included
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/nr_sw_semantic_templates/pi1/class.tx_nrswsemantictemplates_pi1.php'])	{
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/nr_sw_semantic_templates/pi1/class.tx_nrswsemantictemplates_pi1.php']);
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/nr_sw_semantic_templates/pi1/class.tx_nrswsemantictemplates_pi1.php']) {
+    include_once $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/nr_sw_semantic_templates/pi1/class.tx_nrswsemantictemplates_pi1.php'];
 }
 
 ?>
