7ed1d61443c3eae50a0d68aabdfa628b57b36b95
Christian Weiske <christian.weiske@netresearch.de>
2011-05-12 13:02:09 +0200

    WKP-45: begin context sensitive help

--- ext_tables.php
+++ ext_tables.php
@@ -23,6 +23,10 @@ t3lib_extMgm::addPiFlexFormValue(
     $_EXTKEY . '_pi1',
     'FILE:EXT:' . $_EXTKEY . '/flexform_ds_pi1.xml'
 );
+t3lib_extMgm::addLLrefForTCAdescr(
+    'tt_content.pi_flexform.nr_semantic_templates_pi1.list',
+    'EXT:nr_semantic_templates/res/locallang_csh.xml'
+);
 
 if (TYPO3_MODE == 'BE') {
     $TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']
--- lib/class.tx_nrsemantictemplates_wizicon.php
+++ lib/class.tx_nrsemantictemplates_wizicon.php
@@ -24,43 +24,43 @@
  */
 class tx_nrsemantictemplates_wizicon
 {
-	/**
-	 * Adds the formhandler wizard icon
-	 *
-	 * @param array $wizardItems Input array with wizard items for plugins
+    /**
+     * Adds the formhandler wizard icon
      *
-	 * @return array Modified input array, having the semantic template
+     * @param array $wizardItems Input array with wizard items for plugins
+     *
+     * @return array Modified input array, having the semantic template
      *               pi1 item added.
-	 */
-	public function proc($wizardItems)
-	{
-		global $LANG;
-
-		$LL = $this->includeLocalLang();
-
-		$wizardItems['plugins_tx_formhandler_pi1'] = array(
-			'icon'        => t3lib_extMgm::extRelPath('nr_semantic_templates')
-                . 'res/ce_wiz_pi1.png',
-			'title'       => $LANG->getLLL('tt_content.list_type_pi1', $LL),
-			'description' => $LANG->getLLL('tt_content.wiz_description_pi1',$LL),
-			'params'      => '&defVals[tt_content][CType]=list'
-                . '&defVals[tt_content][list_type]=nr_semantic_templates_pi1'
-		);
-
-		return $wizardItems;
-	}
-	
-	/**
-	 * Includes the locallang file for this extension
-	 *
-	 * @return array The LOCAL_LANG array
-	 */
-	protected function includeLocalLang()
+     */
+    public function proc($wizardItems)
     {
-		$llFile= t3lib_extMgm::extPath('nr_semantic_templates')
+        global $LANG;
+
+        $LL = $this->includeLocalLang();
+
+        $wizardItems['plugins_tx_formhandler_pi1'] = array(
+            'icon'        => t3lib_extMgm::extRelPath('nr_semantic_templates')
+                . 'res/ce_wiz_pi1.png',
+            'title'       => $LANG->getLLL('tt_content.list_type_pi1', $LL),
+            'description' => $LANG->getLLL('tt_content.wiz_description_pi1',$LL),
+            'params'      => '&defVals[tt_content][CType]=list'
+                . '&defVals[tt_content][list_type]=nr_semantic_templates_pi1'
+        );
+
+        return $wizardItems;
+    }
+    
+    /**
+     * Includes the locallang file for this extension
+     *
+     * @return array The LOCAL_LANG array
+     */
+    protected function includeLocalLang()
+    {
+        $llFile= t3lib_extMgm::extPath('nr_semantic_templates')
             . 'locallang_db.xml';
-		return t3lib_div::readLLXMLfile($llFile, $GLOBALS['LANG']->lang);
-	}
+        return t3lib_div::readLLXMLfile($llFile, $GLOBALS['LANG']->lang);
+    }
 
 }
 
