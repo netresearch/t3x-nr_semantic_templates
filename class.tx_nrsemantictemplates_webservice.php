e6d82102d1d855377fbc7b66796a64d387ead66e
Christian Weiske <christian.weiske@netresearch.de>
2011-05-12 14:19:45 +0200

    WKP-43: refactor/reformat webservice

--- class.tx_nrsemantictemplates_webservice.php
+++ class.tx_nrsemantictemplates_webservice.php
@@ -1,15 +1,15 @@
 <?php
+declare(encoding = 'utf-8');
 /**
  * Semantic Templates Typo3 extension.
  *
  * PHP version 5
  *
- * @category   Netresearch
- * @package    TYPO3
- * @subpackage nr_semantic_templates
- * @author     Raphael Doehring <raphael.doehring@netresearch.de>
- * @license    AGPL v3 or later http://www.gnu.org/licenses/agpl.html
- * @link       http://www.netresearch.de
+ * @category Netresearch
+ * @package  nr_semantic_templates
+ * @author   Raphael Doehring <raphael.doehring@netresearch.de>
+ * @license  AGPL v3 or later http://www.gnu.org/licenses/agpl.html
+ * @link     http://www.netresearch.de/
  */
 require_once dirname(__FILE__) . '/lib/class.tx_nrsemantictemplates_config.php';
 
@@ -19,12 +19,11 @@ require_once dirname(__FILE__) . '/lib/class.tx_nrsemantictemplates_config.php';
  *
  * Used from within the flexform configuration
  *
- * @category   Netresearch
- * @package    TYPO3
- * @subpackage nr_semantic_templates
- * @author     Raphael Doehring <raphael.doehring@netresearch.de>
- * @license    AGPL v3 or later http://www.gnu.org/licenses/agpl.html
- * @link       http://www.netresearch.de
+ * @category Netresearch
+ * @package  nr_semantic_templates
+ * @author   Raphael Doehring <raphael.doehring@netresearch.de>
+ * @license  AGPL v3 or later http://www.gnu.org/licenses/agpl.html
+ * @link     http://www.netresearch.de/
  */
 class tx_nrsemantictemplates_webservice
 {
@@ -42,9 +41,42 @@ class tx_nrsemantictemplates_webservice
      */
     protected $config = null;
 
+
+
+    /**
+     * Initializes the configuration
+     */
     public function __construct()
     {
         $this->config = t3lib_div::makeInstance('tx_nrsemantictemplates_config');
+    }
+
+
+
+    /**
+     * Updates the config array with an error message in the dropdown select
+     * items, using the old (previous) value as value.
+     *
+     * @param string $errorMsg Error message to show
+     * @param mixed  $oldValue Previously selected option value
+     * @param array  $config   Configuration array that needs to be modified and
+     *                         returned
+     *
+     * @return array Modified $coonfig array.
+     */
+    protected function returnErrorInSelect($errorMsg, $oldValue, $config)
+    {
+        $config['items'] = array_merge(
+            $config['items'],
+            array(
+                array(
+                    0 => $errorMsg,
+                    1 => $oldValue
+                )
+            )
+        );
+        sort($config['items']);
+        return $config;
     }
 
 
@@ -62,98 +94,115 @@ class tx_nrsemantictemplates_webservice
     {
         $this->config->setFlexformFromRowConfig($config['row']);
 
-        $lessUrl = $this->config->get('lessUrl', null, 'sBasic');
         $oldTemplateValue = $this->config->get('templateId');
+        $lessUrl          = $this->config->get('lessUrl', null, 'sBasic');
 
-        $optionList = array();
-        if (filter_var($lessUrl, FILTER_VALIDATE_URL)) {
-            $lessUrl = $this->_appendSlash($lessUrl);
-
-            // test web service response/reachability
-            if ($this->_isValidWebservice($lessUrl)) {
-                $jsonContent = file_get_contents($lessUrl . 'service/list');
-                $templatesArray = json_decode($jsonContent);
-
-                // build select box options array
-                if (is_array($templatesArray)) {
-                    foreach ($templatesArray as $currentTemplate) {
-
-                        // @-thing is a hack to get the request type an the id
-                        // in one select box
-                        $value = $currentTemplate->requestType . '@' . $currentTemplate->id;
-                        $optionList[] = array(0 => $currentTemplate->name,  1 => $value);
-                    } // -- foreach template in array
-                } else {
-                    $optionList[] = array(0 => 'ERROR: Got no data from web serivce.',  1 => $oldTemplateValue);
-                } // -- response array is empty
-            } else {
-                $optionList[] = array(0 => 'ERROR: Server not reachable or not a LESS instance.',  1 => $oldTemplateValue);
-            } // -- else, web service response is not as expected
-        } else {
-            //URL validation failed
-            $optionList[] = array(
-                0 => 'ERROR: URL is not valid.',
-                1 => $oldTemplateValue
+        if (!filter_var($lessUrl, FILTER_VALIDATE_URL)) {
+            return $this->returnErrorInSelect(
+                'ERROR: URL is not valid.', $oldTemplateValue, $config
             );
         }
+
+        $lessUrl = $this->_appendSlash($lessUrl);
+        // test web service response/reachability
+        if (!$this->isValidWebservice($lessUrl)) {
+            return $this->returnErrorInSelect(
+                'ERROR: Server not reachable or not a LESS instance.',
+                $oldTemplateValue, $config
+            );
+        }
+
+        $jsonContent    = file_get_contents($lessUrl . 'service/list');
+        $templatesArray = json_decode($jsonContent);
+
+        // build select box options array
+        if (!is_array($templatesArray)) {
+            return $this->returnErrorInSelect(
+                'ERROR: Got no data from web serivce.',
+                $oldTemplateValue, $config
+            );
+        }
+
+        $optionList = array();
+        foreach ($templatesArray as $currentTemplate) {
+            // @-thing is a hack to get the request type an the id
+            // in one select box
+            $value = $currentTemplate->requestType . '@' . $currentTemplate->id;
+            $optionList[] = array(0 => $currentTemplate->name,  1 => $value);
+        } // -- foreach template in array
 
         $config['items'] = array_merge($config['items'], $optionList);
         sort($config['items']);
         return $config;
+    }
 
-    } // -- function getTemplateNames
 
 
     /**
      * Gets the list of available template versions for a certain id from the
      * webservice.
      *
-     * @param mixed $config the typo3 config
+     * @param array $config the typo3 config
      *
-     * @return mixed the config with added options for the select box
+     * @return array The config with added options for the select box
      */
     public function getTemplateVersions($config)
     {
         $this->config->setFlexformFromRowConfig($config['row']);
         $templateIdString = $this->config->get('templateId');
+        $oldVersionValue  = $this->config->get('templateVersion');
 
-        $parts = split('@', $templateIdString);
+        $parts = explode('@', $templateIdString);
         if (!is_array($parts) || count($parts) !== 2) {
-            return '';
+            return $this->returnErrorInSelect(
+                'Invalid template', $oldVersionValue, $config
+            );
         }
 
         $templateId = $parts[1];
-
-        $lessUrl = $this->config->get('lessUrl', null, 'sBasic');
-
+        $lessUrl    = $this->config->get('lessUrl', null, 'sBasic');
         if ('' === $templateId || '' === $lessUrl) {
-            return '';
+            return $this->returnErrorInSelect(
+                'Invalid Template or LESS URL', $oldVersionValue, $config
+            );
         }
 
         $versionsArray = null;
-        if (filter_var($lessUrl, FILTER_VALIDATE_URL)) {
-            $lessUrl = $this->_appendSlash($lessUrl);
-            if ($this->_isValidWebservice($lessUrl)) {
-                $jsonContent = file_get_contents(
-                    $lessUrl . 'service/template-versions?templateId=' . $templateId
-                );
-                $versionsArray = json_decode($jsonContent);
-
-            }
+        if (!filter_var($lessUrl, FILTER_VALIDATE_URL)) {
+            return $this->returnErrorInSelect(
+                'Invalid LESS URL', $oldVersionValue, $config
+            );
         }
 
-        $optionList = array();
-        if (is_array($versionsArray) && count($versionsArray) > 0) {
+        $lessUrl = $this->_appendSlash($lessUrl);
+        if (!$this->isValidWebservice($lessUrl)) {
+            return $this->returnErrorInSelect(
+                'ERROR: Server not reachable or not a LESS instance.',
+                $oldTemplateValue, $config
+            );
+        }
+
+        $jsonContent = file_get_contents(
+            $lessUrl . 'service/template-versions?templateId=' . $templateId
+        );
+        $versionsArray = json_decode($jsonContent);
+        if (!is_array($versionsArray)) {
+            return $this->returnErrorInSelect(
+                'ERROR: Got no data from web serivce.',
+                $oldTemplateValue, $config
+            );
+        }
+
+        $optionList  = array();
+        if (count($versionsArray) > 0) {
             foreach ($versionsArray as $versionNumber) {
                 $optionList[] = array(0 => $versionNumber,  1 => $versionNumber);
             }
-
-            $config['items'] = array_merge($config['items'], $optionList);
-            return $config;
         }
 
-        return '';
-    } // -- function getTemplateVersions
+        $config['items'] = array_merge($config['items'], $optionList);
+        return $config;
+    }
 
 
 
@@ -172,7 +221,8 @@ class tx_nrsemantictemplates_webservice
         }
 
         return $url;
-    } // -- function validateAppendSlash
+    }
+
 
 
     /**
@@ -183,7 +233,7 @@ class tx_nrsemantictemplates_webservice
      *
      * @return boolean true if webservice response is as expected, false otherwise
      */
-    private function _isValidWebservice($url)
+    protected function isValidWebservice($url)
     {
         $testContent = file_get_contents($url . 'service/ping');
 
