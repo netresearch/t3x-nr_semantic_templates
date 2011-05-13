6b09cc09850841a5438f89586f5f36a1465f5aed
Christian Weiske <christian.weiske@netresearch.de>
2011-05-13 16:56:22 +0200

    WKP-3: update ext_emconf with information from ter uploaded release

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "nr_semantic_templates"
+# Extension Manager/Repository config file for ext "nr_semantic_templates".
 #
-# Auto generated 16-09-2009 13:50
+# Auto generated 13-05-2011 16:56
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.2.0',
+	'version' => '0.3.0',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"dc72";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"5fc7";s:14:"ext_tables.php";s:4:"6e17";s:16:"locallang_db.xml";s:4:"f5c5";s:19:"doc/wizard_form.dat";s:4:"1de9";s:20:"doc/wizard_form.html";s:4:"711d";s:42:"pi1/class.tx_nrswsemantictemplates_pi1.php";s:4:"7003";s:17:"pi1/locallang.xml";s:4:"b372";}',
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"32e1";s:10:"README.txt";s:4:"c113";s:43:"class.tx_nrsemantictemplates_webservice.php";s:4:"10c2";s:21:"ext_conf_template.txt";s:4:"b416";s:12:"ext_icon.gif";s:4:"2cc8";s:17:"ext_localconf.php";s:4:"dc2b";s:14:"ext_tables.php";s:4:"2ecd";s:19:"flexform_ds_pi1.xml";s:4:"4ae9";s:16:"locallang_db.xml";s:4:"be3d";s:8:"doc/TODO";s:4:"8971";s:14:"doc/manual.pdf";s:4:"a59e";s:14:"doc/manual.sxw";s:4:"468e";s:46:"lib/class.tx_nrsemantictemplates_bepreview.php";s:4:"50a1";s:43:"lib/class.tx_nrsemantictemplates_config.php";s:4:"b3c1";s:44:"lib/class.tx_nrsemantictemplates_wizicon.php";s:4:"f277";s:40:"pi1/class.tx_nrsemantictemplates_pi1.php";s:4:"ce5a";s:18:"res/ce_wiz_pi1.png";s:4:"2400";s:21:"res/locallang_csh.xml";s:4:"44a4";}',
+	'suggests' => array(
+	),
 );
 
 ?>
